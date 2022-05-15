6754ad185acf38d8b4dec774daaec732c2d8b62d
Bimbola Lasisi <78049244+blasisi@users.noreply.github.com>
2022-05-15 14:31:13 +0100

    added components

---
+++ scrum-app/src/components/Header.js
@@ -0,0 +1,12 @@
+// import React from "react";
+// import { Link } form "react-router-dom";
+
+// function Header(){
+
+//     return (
+
+//         <div id="header">
+
+//     )
+// }
+export default Headers;
